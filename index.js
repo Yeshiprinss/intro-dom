ddb997ab0a85ee2f7744b97835b08142a01c1b55
Yessenia Martinez <148432911+Yeshiprinss@users.noreply.github.com>
2023-11-28 12:23:19 +0100

    add exercise DOM

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function change() {
-    
-}
+        let redSquareDiv = document.querySelector('.redSquare');
+        let newRedSquare = document.createElement('div');
+        newRedSquare.classList.add('red-square');
+        redSquareDiv.appendChild(newRedSquare);
+      }
